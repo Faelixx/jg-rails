2203c95a73a5f67d0e5a3a4f3c96e7ac202b129e
Faelixx <sasha.taylor7@gmail.com>
2024-04-14 16:48:00 -0600

    Feat: sign up page added

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,30 @@
+class Admin::CategoriesController < ApplicationController
+  username = ENV['USER']
+  password = ENV['PASS']
+  http_basic_authenticate_with name: username, password: password
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
+  resources :about, only: [:index]
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -12,11 +14,12 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  resources :about, only: [:index]
+  resources :user, only: [:index, :create, :new]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
